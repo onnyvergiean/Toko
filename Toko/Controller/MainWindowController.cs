6f13f874c901eff4e049b24e1d64efc0667c08ad
onny <onny.s@students.amikom.ac.id>
2021-01-01 21:59:38 +0700

    Add Controller and Logic

---
+++ Toko/Controller/MainWindowController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Toko.Model;
+
+namespace Toko.Controller
+{
+    
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+
+        public void removeItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+
+        public List<Item> getItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+    }
+}
