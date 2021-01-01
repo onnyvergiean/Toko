5cff8034c0d1dea14269d05858c7dbaca8a0bda5
onny <onny.s@students.amikom.ac.id>
2021-01-01 22:47:08 +0700

    add Promo xaml dan class

---
+++ Toko/Promo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Toko
+{
+    /// <summary>
+    /// Interaction logic for Promo.xaml
+    /// </summary>
+    public partial class Promo : Window
+    {
+        public Promo()
+        {
+            InitializeComponent();
+        }
+    }
+}
