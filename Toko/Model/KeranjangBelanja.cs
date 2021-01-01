f79ca6523620082fa5c1e6cd1687dbfb9d7b047a
onny <onny.s@students.amikom.ac.id>
2021-01-01 21:59:27 +0700

    Add Model Class and Logic

---
+++ Toko/Model/KeranjangBelanja.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toko.Model
+{
+    class KeranjangBelanja
+    {
+        List<Item> itemkeranjangBelanja;
+        onKeranjangBelanjaChangedListener onKeranjangBelanjaChangedListener;
+
+        public KeranjangBelanja(onKeranjangBelanjaChangedListener onKeranjangBelanjaChangedListener)
+        {
+            this.onKeranjangBelanjaChangedListener = onKeranjangBelanjaChangedListener;
+            this.itemkeranjangBelanja = new List<Item>();
+        }
+        public List<Item> getItems()
+        {
+            return this.itemkeranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.itemkeranjangBelanja.Add(item);
+            this.onKeranjangBelanjaChangedListener.addItemSucceed();
+            calculateSubTotal();
+        }
+
+        public void removeItem(Item item)
+        {
+            this.itemkeranjangBelanja.Remove(item);
+            this.onKeranjangBelanjaChangedListener.removeItemSucceed();
+            calculateSubTotal();
+        }
+
+        private void calculateSubTotal()
+        {
+            double subTotal = 0;
+            foreach(Item item in itemkeranjangBelanja)
+            {
+                subTotal += item.price;
+            }
+        }
+    }
+
+   
+
+    interface onKeranjangBelanjaChangedListener
+    {
+        void removeItemSucceed();
+        void addItemSucceed();
+    }
+}
---
+++ Toko/Model/Promo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Toko.Model
+{
+    public class Promo
+    {
+        public string promo { get; set; }
+        public double diskon { get; set; }
+
+        public Promo(string promo, double diskon)
+        {
+            this.promo = promo;
+            this.diskon = diskon;
+        }
+    }
+}
