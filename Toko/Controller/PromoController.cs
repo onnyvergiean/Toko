478ac609ea169d73d0af5ffe7febe7209412770c
onny <onny.s@students.amikom.ac.id>
2021-01-02 09:24:25 +0700

    Add function addPromo and getPromo

--- Toko/Controller/PromoController.cs
+++ Toko/Controller/PromoController.cs
@@ -7,5 +7,20 @@ namespace Toko.Controller
     class PromoController
     {
         private List<Promo> promo;
+
+        public PromoController()
+        {
+            promo = new List<Promo>();
+        }
+
+        public void addPromo(Promo promo)
+        {
+            this.promo.Add(promo);
+        }
+
+        public List<Promo> getPromo()
+        {
+            return this.promo;
+        }
     }
 }
