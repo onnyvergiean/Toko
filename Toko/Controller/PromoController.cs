0739bc2835e0434c37442d9748abeea11c4ba21d
onny <onny.s@students.amikom.ac.id>
2021-01-02 09:18:40 +0700

    Add PromoController

---
+++ Toko/Controller/PromoController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toko.Controller
+{
+    class PromoController
+    {
+        private List<Promo> promo;
+    }
+}
